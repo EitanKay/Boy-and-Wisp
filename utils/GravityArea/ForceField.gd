057a375d32bbc17b427093c64e31831bab5b5182
Eitan <eitan.kayesar@mail.huji.ac.il>
2025-06-14 22:25:53 +0300

    fixed force field

--- utils/GravityArea/ForceField.gd
+++ utils/GravityArea/ForceField.gd
@@ -16,12 +16,14 @@ func _process(delta: float) -> void:
 	for body in affectedBodies:
 		var direction = (body.global_position - global_position).normalized()
 		var distance_squared : float = (body.global_position - global_position).length()
+		print("applying force on" , body)
 		body.apply_central_force(direction * force_field_strength / distance_squared)
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is RigidBody2D and (body != exclude_body):
 		print( "Detected : " ,body)
+		affectedBodies.append(body)
 
 
 func _on_body_exited(body: Node2D) -> void:
